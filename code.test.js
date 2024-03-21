89245329c3f0408c51fdf8660cdfbc7b4a7a48cc
Caden Mcfate <113316495+cadenmcfate@users.noreply.github.com>
2024-03-20 18:48:26 -0600

    typo

--- code.test.js
+++ code.test.js
@@ -5,7 +5,7 @@ eval(fs.readFileSync('code.js')+'');
 
 function controlFib(n) { // from the slides
     if (n <= 2) return 1;
-    else        return controlFib(n-1) + fib(n-2);
+    else        return controlFib(n-1) + controlFib(n-2);
 }
 
 for (let i = 0; i < 100; i++) {
