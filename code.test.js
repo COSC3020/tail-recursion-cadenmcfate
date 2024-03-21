2131f4e3df8d54c8f3c74f2bb1ddc4b354d4bad1
Caden McFate <cadenmcfate@gmail.com>
2024-03-20 18:22:49 -0600

    implimented tail-recursive fibonacci & tests

---
+++ code.test.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const assert = require('assert');
+
+eval(fs.readFileSync('code.js')+'');
+
+function controlFib(n) { // from the slides
+    if (n <= 2) return 1;
+    else        return controlFib(n-1) + fib(n-2);
+}
+
+for (let i = 0; i < 100; i++) {
+    assert(fib(i) == controlFib(i));
+}
